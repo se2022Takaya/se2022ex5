63a89c40200020949e95f93a648f11c356e63593
takaya <r202103304nu@jindai.jp>
2023-01-26 23:47:17 +0900

    ex version1

---
+++ forloop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+
+    for(int i= 0; i<5;i++){
+        printf("i is smaller than 5");
+    }
+    return 0;
+}
---
+++ number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int x = 5;
+    int y = 3;
+
+    printf("x + y = %d", x+y);
+    printf("x - y = %d", x-y);
+    return 0;
+}
